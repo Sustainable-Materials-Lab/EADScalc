3a5b320241b6005e4256cd03ac477c7d107eb7d4
Samuel Eyley <samuel.eyley@kuleuven.be>
2025-10-31 09:56:14 +0100

    feat: added __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,20 @@
+"""
+EADScalc - Elemental Analysis and XPS Data Calculator for Cellulose
+
+This package provides tools for calculating the degree of substitution (DS) 
+for modified cellulose samples based on elemental analysis (CHNS) data and 
+XPS (X-ray Photoelectron Spectroscopy) data.
+
+Modules:
+    EADScalc: Calculate DS from elemental analysis (CHNS) data
+    XPSDScalc: Calculate DS from XPS atomic concentration data
+"""
+
+__version__ = "1.0.0"
+__author__ = "Sam Eyley"
+
+# Import main CLI functions for convenience
+from .EADScalc import cli as eads_cli
+from .XPSDScalc import *
+
+__all__ = ['eads_cli']
